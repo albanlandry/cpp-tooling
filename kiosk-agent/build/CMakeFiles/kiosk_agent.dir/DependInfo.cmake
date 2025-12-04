daac804ac9c2ff00939bd46d1027c5411d3a975b
albanlandry <38934849+albanlandry@users.noreply.github.com>
2025-12-04 19:45:00 +0900

    Updated

---
+++ kiosk-agent/build/CMakeFiles/kiosk_agent.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Volumes/Storage/cpp/devOS/kiosk-agent/src/lib/CardReaderControl.cpp" "CMakeFiles/kiosk_agent.dir/src/lib/CardReaderControl.cpp.o" "gcc" "CMakeFiles/kiosk_agent.dir/src/lib/CardReaderControl.cpp.o.d"
+  "/Volumes/Storage/cpp/devOS/kiosk-agent/src/lib/CommandHandler.cpp" "CMakeFiles/kiosk_agent.dir/src/lib/CommandHandler.cpp.o" "gcc" "CMakeFiles/kiosk_agent.dir/src/lib/CommandHandler.cpp.o.d"
+  "/Volumes/Storage/cpp/devOS/kiosk-agent/src/lib/Config.cpp" "CMakeFiles/kiosk_agent.dir/src/lib/Config.cpp.o" "gcc" "CMakeFiles/kiosk_agent.dir/src/lib/Config.cpp.o.d"
+  "/Volumes/Storage/cpp/devOS/kiosk-agent/src/lib/Metrics.cpp" "CMakeFiles/kiosk_agent.dir/src/lib/Metrics.cpp.o" "gcc" "CMakeFiles/kiosk_agent.dir/src/lib/Metrics.cpp.o.d"
+  "/Volumes/Storage/cpp/devOS/kiosk-agent/src/lib/UnityControl.cpp" "CMakeFiles/kiosk_agent.dir/src/lib/UnityControl.cpp.o" "gcc" "CMakeFiles/kiosk_agent.dir/src/lib/UnityControl.cpp.o.d"
+  "/Volumes/Storage/cpp/devOS/kiosk-agent/src/lib/mqtt/MqttClient.cpp" "CMakeFiles/kiosk_agent.dir/src/lib/mqtt/MqttClient.cpp.o" "gcc" "CMakeFiles/kiosk_agent.dir/src/lib/mqtt/MqttClient.cpp.o.d"
+  "/Volumes/Storage/cpp/devOS/kiosk-agent/src/lib/utils/JsonUtils.cpp" "CMakeFiles/kiosk_agent.dir/src/lib/utils/JsonUtils.cpp.o" "gcc" "CMakeFiles/kiosk_agent.dir/src/lib/utils/JsonUtils.cpp.o.d"
+  "/Volumes/Storage/cpp/devOS/kiosk-agent/src/main.cpp" "CMakeFiles/kiosk_agent.dir/src/main.cpp.o" "gcc" "CMakeFiles/kiosk_agent.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ kiosk-agent/build/CMakeFiles/kiosk_agent.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kiosk_agent.dir/src/lib/CardReaderControl.cpp.o"
+  "CMakeFiles/kiosk_agent.dir/src/lib/CardReaderControl.cpp.o.d"
+  "CMakeFiles/kiosk_agent.dir/src/lib/CommandHandler.cpp.o"
+  "CMakeFiles/kiosk_agent.dir/src/lib/CommandHandler.cpp.o.d"
+  "CMakeFiles/kiosk_agent.dir/src/lib/Config.cpp.o"
+  "CMakeFiles/kiosk_agent.dir/src/lib/Config.cpp.o.d"
+  "CMakeFiles/kiosk_agent.dir/src/lib/Metrics.cpp.o"
+  "CMakeFiles/kiosk_agent.dir/src/lib/Metrics.cpp.o.d"
+  "CMakeFiles/kiosk_agent.dir/src/lib/UnityControl.cpp.o"
+  "CMakeFiles/kiosk_agent.dir/src/lib/UnityControl.cpp.o.d"
+  "CMakeFiles/kiosk_agent.dir/src/lib/mqtt/MqttClient.cpp.o"
+  "CMakeFiles/kiosk_agent.dir/src/lib/mqtt/MqttClient.cpp.o.d"
+  "CMakeFiles/kiosk_agent.dir/src/lib/utils/JsonUtils.cpp.o"
+  "CMakeFiles/kiosk_agent.dir/src/lib/utils/JsonUtils.cpp.o.d"
+  "CMakeFiles/kiosk_agent.dir/src/main.cpp.o"
+  "CMakeFiles/kiosk_agent.dir/src/main.cpp.o.d"
+  "kiosk_agent"
+  "kiosk_agent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kiosk_agent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
